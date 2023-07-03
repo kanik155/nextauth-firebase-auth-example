36cf561c59e7851ec198ae3ba7797d65561b010d
Ken Taketani <takekanik155@gmail.com>
2023-07-04 02:31:01 +0900

    update

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth"
+import { JWT } from "next-auth/jwt"
 import { auth } from "firebase-admin"
 
 declare module "next-auth" {
   interface User extends auth.DecodedIdToken {}
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    idToken?: string
+  }
 }
